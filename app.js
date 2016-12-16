d8ed7dbfc446428254d3a267557e0eabd1f14d6a
vedang <vedangdate@gmail.com>
2016-12-16 17:51:40 +0530

    Successful trial

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var restify = require('restify');
 var builder = require('botbuilder');
-
+require('dotenv').load();
 //=========================================================
 // Bot Setup
 //=========================================================
